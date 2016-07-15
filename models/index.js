6c69da73642dfab28797a96f0985951128f3f926
mehgellan <baumegg@gmail.com>
2016-07-15 14:04:42 -0700

    set up heroku mongolab addon

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
+mongoose.connect( process.env.MONGODB_URI || "https://stark-fjord-10734.herokuapp.com/");
 
 // module.exports.Campsite = require("./campsite.js.example");
