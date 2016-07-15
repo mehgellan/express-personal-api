7d435b1fe10d33c16b54c25b0ea374ae858b4a66
mehgellan <baumegg@gmail.com>
2016-07-15 15:04:14 -0700

    update documentation_url and base_url in server.js

--- server.js
+++ server.js
@@ -47,14 +47,14 @@ app.get('/api', function api_index(req, res) {
   res.json({
     woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentation_url: "https://github.com/mehgellan/express-personal-api/README.md", // CHANGE ME
+    base_url: "http://stark-fjord-10734.herokuapp.com", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
       {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
     ]
-  })
+  });
 });
 
 /**********
