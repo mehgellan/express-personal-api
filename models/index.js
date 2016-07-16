916fe92374d378ae4192291792d5874311574b50
mehgellan <baumegg@gmail.com>
2016-07-15 17:09:05 -0700

    create /api/profile response JSON

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "https://stark-fjord-10734.herokuapp.com/");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
 
 // module.exports.Campsite = require("./campsite.js.example");
