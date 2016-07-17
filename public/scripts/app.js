bfd752765687b8ef45c5f7c556849f6fd0143228
mehgellan <baumegg@gmail.com>
2016-07-17 14:48:06 -0700

    attempt to get handlebars to work

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,49 @@
 console.log("Sanity Check: JS is working!");
+var template;
+var $albumsList;
+var allAlbums = [];
 
 $(document).ready(function(){
+  $albumsList = $('#target');
+  var source = $('#albums-template').html();
+  template = Handlebars.compile(source);
 
-// your code
+  $.ajax({
+    method: 'GET',
+    url: 'api/albums',
+    success: onSuccess,
+    error: onError
+  });
+
+  $('#newAlbumForm').on('submit', function(e) {
+    e.preventDefault();
+    $.ajax({
+      method: 'POST',
+      url: '/api/albums',
+      data: $(this).serialize(),
+      success: newAlbumSuccess,
+      error: newAlbumError
+    });
+  });
 
 });
+
+function render() {
+  $albumsList.empty();
+  var albumsHtml = template({albums: allAlbums});
+  $albumsList.append(albumsHtml);
+}
+
+function onSuccess(json) {
+  allAlbums = json;
+  render();
+}
+
+function onError() {
+  console.log('ajax get request error');
+  $albumsList.text('Failed to load albums, check the server');
+}
+
+function newAlbumSuccess(json) {
+  $('#newAlbumForm input').val('');
+}
